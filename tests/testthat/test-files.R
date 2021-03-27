6f98ba9dd855d7b5d5b7d11f7eb8d1655442bd7b
Jordan Ayala <jmayala@protonmail.com>
2021-03-27 11:28:03 -0300

    Augment test case

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -1,9 +1,9 @@
 context("Test files.R")
 
 test_that("auto_group_files should group files by file name prefix", {
-  files <- c("AAPL_1.csv", "AAPL_2.csv", "AMZN_1.csv", "AMZN_2.csv")
+  files <- c("AAPL-1.csv", "AAPL-2.csv", "AMZN_1.csv", "AMZN_2.csv", "IG_AUDJPY_D1-1.csv", "IG_AUDJPY_D1-2.csv", "IG_EURUSD_D1_1.csv", "IG_EURUSD_D1_2.csv")
   grouped <- auto_group_files(files)
-  expect_equal(grouped$dataset, c("AAPL", "AAPL", "AMZN", "AMZN"))
+  expect_equal(grouped$dataset, c("AAPL", "AAPL", "AMZN", "AMZN", "IG_AUDJPY_D1", "IG_AUDJPY_D1", "IG_EURUSD_D1", "IG_EURUSD_D1"))
 })
 
 test_that("group_files should group files by the same dataset", {
